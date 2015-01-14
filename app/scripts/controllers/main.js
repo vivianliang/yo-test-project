048c3b2c3f39e368d4f322d6a82cba4e4ff25af4
Vivian Liang <vliang88@gmail.com>
2015-01-14 15:24:34 -0800

    Make todos persist with local storage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,23 @@
  * Controller of the yoTestProjectApp
  */
 angular.module('yoTestProjectApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function(){
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
     $scope.addTodo = function(){
       $scope.todos.push($scope.todo);
       $scope.todo = '';
     };
+
     $scope.removeTodo = function(index){
       $scope.todos.splice(index,1);
     };
+
   });
